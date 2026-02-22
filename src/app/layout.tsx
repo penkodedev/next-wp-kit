9275d5ebb7a3c4648ac2f50f0a6e4701cc6bed75
rickramalho <email@penkode.com>
2026-02-22 14:02:15 +0100

    Test: uncomment components28

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -197,7 +197,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
             }}
           >
             <WpPageIdProvider>
-              {/* <ScrollProgress /> */}
+              <ScrollProgress />
               {/* <SmoothScroll> */}
                 {/* <ParallaxEffects /> */}
                 {/* <BodyClass> */}
